8a2c3dd7008190aa21a5d546785c16cc94b015c1
Urska Pangerc <urska.pangerc@gmail.com>
2018-01-18 10:43:39 +0100

    add name to group members

--- src/main/java/com/tracker/apientities/organizationgroup/APIUserGroupRolesDetail.java
+++ src/main/java/com/tracker/apientities/organizationgroup/APIUserGroupRolesDetail.java
@@ -6,10 +6,12 @@ public class APIUserGroupRolesDetail {
 	public String userId;
 	public Boolean isAdmin;
 	public Boolean isUser;
+	public String name;
 	
 	public APIUserGroupRolesDetail(GroupRoles role) {
 		this.userId = role.getUser().getUserId();
 		this.isAdmin = role.isAdminRole();
 		this.isUser = role.isUserRole();
+		this.name = role.getUser().name;
 	}
 }
